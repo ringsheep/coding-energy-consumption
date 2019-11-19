63686ae0bee56dd317c0bd0556bd6e98c8f83355
George Zinyakov <gzinyakov@alfabank.ru>
2019-11-20 02:02:47 +0300

    inital commit

---
+++ src/main/java/org/ziniakov/codingenergyconsumption/CodingEnergyConsumptionApplication.java
@@ -0,0 +1,13 @@
+package org.ziniakov.codingenergyconsumption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingEnergyConsumptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingEnergyConsumptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ziniakov/codingenergyconsumption/CodingEnergyConsumptionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ziniakov.codingenergyconsumption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingEnergyConsumptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

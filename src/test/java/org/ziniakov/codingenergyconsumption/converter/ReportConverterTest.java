b07f9fa3371449f760bbcec32788c8846cda0bbe
George Zinyakov <gzinyakov@alfabank.ru>
2019-11-21 03:34:35 +0300

    fix: optimize imports

--- src/test/java/org/ziniakov/codingenergyconsumption/converter/ReportConverterTest.java
+++ src/test/java/org/ziniakov/codingenergyconsumption/converter/ReportConverterTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ReportConverterTest {
 
